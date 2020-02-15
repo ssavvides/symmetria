5ddf02e297c72e563585a16c6bd618f7ff3c541c
Savvas Savvides <savvas@purdue.edu>
2020-02-14 19:49:59 -0500

    packed paillier

---
+++ src/main/java/edu/purdue/symmetria/evaluate/PackedScheme.java
@@ -0,0 +1,142 @@
+package edu.purdue.symmetria.evaluate;
+
+import edu.purdue.symmetria.crypto.Paillier;
+import edu.purdue.symmetria.crypto.SymAHE;
+import edu.purdue.symmetria.crypto.cipher.SymCipher;
+import edu.purdue.symmetria.utils.MathUtils;
+
+import java.math.BigInteger;
+
+public class AHEScheme {
+
+    private static final int WARMUP = 10;
+    private static final int ITERATIONS = 100;
+
+    private static SymAHE symAHE = new SymAHE();
+    private static Paillier paillier = new Paillier();
+
+    public enum AHEOp {
+        ENCRYPT, DECRYPT, ADD, ADD_PLAINTEXT, SUBTRACT, MULTIPLY, NEGATE
+    }
+
+    private static void timeOp(AHEOp op) {
+
+        System.out.println("\nEvaluating " + op.name() + " ...");
+
+        long startTime;
+        long symAHETime = 0;
+        long paillierTime = 0;
+        for (int i = 0; i < ITERATIONS + WARMUP; i++) {
+            long m1 = MathUtils.randLong(1_000_000);
+            long m2 = MathUtils.randLong(1_000_000);
+
+            SymCipher c_ahe1 = symAHE.encrypt(m1);
+            SymCipher c_ahe2 = symAHE.encrypt(m2);
+            BigInteger c_paillier1 = paillier.encrypt(m1);
+            BigInteger c_paillier2 = paillier.encrypt(m2);
+
+            switch (op) {
+                case ENCRYPT:
+                    startTime = System.nanoTime();
+                    symAHE.encrypt(m1);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.encrypt(m1);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case DECRYPT:
+                    startTime = System.nanoTime();
+                    symAHE.decrypt(c_ahe1);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.decrypt(c_paillier1);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case ADD:
+                    startTime = System.nanoTime();
+                    symAHE.add(c_ahe1, c_ahe2);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.add(c_paillier1, c_paillier2);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case ADD_PLAINTEXT:
+                    startTime = System.nanoTime();
+                    symAHE.addPlaintext(c_ahe1, m2);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.addPlaintext(c_paillier1, m2);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case SUBTRACT:
+                    startTime = System.nanoTime();
+                    symAHE.subtract(c_ahe1, c_ahe2);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+                    startTime = System.nanoTime();
+
+                    paillier.subtract(c_paillier1, c_paillier2);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case MULTIPLY:
+                    startTime = System.nanoTime();
+                    symAHE.multiply(c_ahe1, m2);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.multiply(c_paillier1, m2);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                case NEGATE:
+                    startTime = System.nanoTime();
+                    symAHE.negate(c_ahe1);
+                    if (i >= WARMUP)
+                        symAHETime += (System.nanoTime() - startTime);
+
+                    startTime = System.nanoTime();
+                    paillier.negate(c_paillier1);
+                    if (i >= WARMUP)
+                        paillierTime += (System.nanoTime() - startTime);
+                    break;
+
+                default:
+                    System.out.println("Unexpected operation");
+                    System.exit(1);
+            }
+        }
+
+        symAHETime = symAHETime / ITERATIONS ;
+        paillierTime = paillierTime / ITERATIONS ;
+
+        System.out.println("SymAHE\tPaillier");
+        System.out.println(symAHETime + "\t" + paillierTime);
+    }
+
+
+    public static void main(String[] args) {
+        for (AHEOp op : AHEOp.values())
+            timeOp(op);
+    }
+
+}
